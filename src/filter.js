30235cdda3bfefef934c0181b331937a70a624cf
Vincent Trekels <vincent@yappa.be>
2017-10-30 12:37:28 +0100

    Added more tests

--- src/filter.js
+++ src/filter.js
@@ -5,17 +5,20 @@ export default (key, context) => {
     translation = window.translations[key];
   }
 
-  const matches = translation.match(/(%([^%]|%%)*%)/g);
-  if (matches) {
-    matches.forEach((match) => {
-      const prop = match.replace(/[%]+/g, '');
-      if (!Object.prototype.hasOwnProperty.call(context, prop)) {
-        return;
-      }
+  if (typeof context === 'object') {
+    const matches = translation.match(/(%([^%]|%%)*%)/g);
 
-      const regex = new RegExp(match, 'g');
-      translation = translation.replace(regex, context[prop]);
-    });
+    if (matches) {
+      matches.forEach((match) => {
+        const prop = match.replace(/[%]+/g, '');
+        if (!Object.prototype.hasOwnProperty.call(context, prop)) {
+          return;
+        }
+
+        const regex = new RegExp(match, 'g');
+        translation = translation.replace(regex, context[prop]);
+      });
+    }
   }
 
   return translation;
--- test/trans.test.js
+++ test/trans.test.js
@@ -3,26 +3,58 @@ import trans from '../src/filter';
 /* eslint-disable */
 
 // Test suite with no resource object
-describe('No translation object provided', () => {
+describe('No global translations object provided', () => {
 
-  it('Should just return the passed key', () => {
-    expect(trans('some_key')).toBe('some_key');
+  it('Should return the passed key if it is undefined', () => {
+    expect(trans('undefined_key')).toBe('undefined_key');
   });
 
 });
 
 // Test suite with global resource object.
-describe('Global translation object as resource', () => {
+describe('Global translations object provided', () => {
 
-  // Providing a set translation keys with corresponding value.
+  // Providing a set of translation keys with corresponding value.
   beforeEach(() => {
     window.translations = {
-      'some_key': 'existing key',
+      'existing_key': 'existing key',
+      'context_key': 'context: %value%',
+      'multiple_context_keys': 'context: %value% %value_two%',
+      'multiple_similar_keys': 'context: %value% %value%',
     };
   });
 
+  it('Should return the passed key if it is undefined', () => {
+    expect(trans('undefined_key')).toBe('undefined_key');
+  });
 
   it('Should return the value if key exists', () => {
-    expect(trans('some_key')).toBe('existing key');
+    expect(trans('existing_key')).toBe('existing key');
   });
+
+  // Test on parsing translations with context
+  describe('translations with context', () => {
+
+    it('Should return the unparsed translation if no context is provided', () => {
+      expect(trans('context_key')).toBe('context: %value%');
+    });
+
+    it('Should return the unparsed translations if the context is empty', () => {
+      expect(trans('context_key', {})).toBe('context: %value%');
+    });
+
+    it('Should return the parsed translations a context is provided', () => {
+      expect(trans('context_key', { value: 'test' })).toBe('context: test');
+    });
+
+    it('Should be able to parse multiple deferring context parameters', () => {
+      expect(trans('multiple_context_keys', { value: 'test', value_two: 'test_two' }))
+        .toBe('context: test test_two');
+    });
+
+    it('Should be able to parse multiple similar context parameters', () => {
+      expect(trans('multiple_similar_keys', { value: 'test' }))
+        .toBe('context: test test');
+    });
+  });  
 });
